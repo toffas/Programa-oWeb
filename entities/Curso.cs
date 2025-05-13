e3b488ebb571ac063e24a0d1f9d06cea99cbd1c8
Gustavo Toffano <168567532+toffas@users.noreply.github.com>
2025-05-12 21:57:29 -0300

    Create Curso.cs

---
+++ entities/Curso.cs
@@ -0,0 +1,27 @@
+namespace MBGestaoEscolarAN.Entities
+{
+    public class Curso
+    {
+        private int cursoId;
+        private string nome;
+        private string codigo;
+        private string descricao;
+        private int cargaHoraria;
+        private string modalidade;
+        private string trilha;
+        private int pontosCap;
+        private string statusCurso;
+        private int coordenadorId;
+
+        public int CursoId { get => cursoId; set => cursoId = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Descricao { get => descricao; set => descricao = value; }
+        public int CargaHoraria { get => cargaHoraria; set => cargaHoraria = value; }
+        public string Modalidade { get => modalidade; set => modalidade = value; }
+        public string Trilha { get => trilha; set => trilha = value; }
+        public int PontosCAP { get => pontosCap; set => pontosCap = value; }
+        public string StatusCurso { get => statusCurso; set => statusCurso = value; }
+        public int CoordenadorId1 { get => coordenadorId; set => coordenadorId = value; }
+    }
+}
