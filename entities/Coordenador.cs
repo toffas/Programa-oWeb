93ff46e4f4358e8b50b0effee953b6dceba1f8f8
Gustavo Toffano <168567532+toffas@users.noreply.github.com>
2025-05-12 21:57:55 -0300

    Create Coordenador.cs

---
+++ entities/Coordenador.cs
@@ -0,0 +1,10 @@
+namespace MBGestaoEscolarAN.Entities
+{
+    public class Coordenador
+    {
+        public int CoodenadorId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
